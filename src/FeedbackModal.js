824c87ce7301002baaea4c9b54d24e65acb1d1c8
Neosoft <neosoft@Neosofts-Mac-mini-7.local>
2019-05-21 19:49:20 +0530

    API integration added

--- src/FeedbackModal.js
+++ src/FeedbackModal.js
@@ -1,11 +1,12 @@
 
 import React, { Component } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input, } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Input } from 'reactstrap';
 
 import ReactCrop from 'react-image-crop';
 import 'react-image-crop/dist/ReactCrop.css';
 import { getCroppedImg } from './Utils/NewCropImage';
 import html2canvas from 'html2canvas';
+import axios from 'axios';
 
 class FeedbackModal extends Component {
     constructor(props) {
@@ -16,28 +17,49 @@ class FeedbackModal extends Component {
             crop: {
             },
             croppedImage: '',
-            showFeedbackModal: false
+            showFeedbackModal: false,
+            fields: {
+                summary: '',
+                description: '',
+                coverPhoto: ''
+            }
         };
         this.screenShotImage = '';
         this.imageRef = '';
         this.toggle = this.toggle.bind(this);
         this.handleOnCropChange = this.handleOnCropChange.bind(this);
-        this.handleImageLoaded = this.handleImageLoaded.bind(this);
         this.handleOnCropComplete = this.handleOnCropComplete.bind(this);
         this.takeScreenShot = this.takeScreenShot.bind(this);
+        this.handleCropImage = this.handleCropImage.bind(this);
 
-        
     }
 
     componentDidMount() {
-        var name = document.getElementById("feedbackId").getAttribute("data-trigger"); 
+        var name = document.getElementById("feedbackId").getAttribute("data-trigger");
         window.$(document).on('click', "." + name, (e) => {
             this.takeScreenShot();
         });
-      
     }
 
     toggle() {
+        this.setState(prevState => ({
+            showFeedbackModal: !prevState.showFeedbackModal
+        }));
+    }
+
+    handleSubmit = () => {
+        const data = {
+            summary: this.state.fields['summary'],
+            description: this.state.fields['description'],
+            coverPhoto: this.state.fields['coverPhoto'],
+        }
+
+        axios.post(`http://192.168.102.120/qa.api.itelbpo/api/issues`, data)
+            .then(res => {
+                const fields = res.data;
+                this.setState({ fields });
+            })
+
         this.setState(prevState => ({
             showFeedbackModal: !prevState.showFeedbackModal
         }));
@@ -48,13 +70,9 @@ class FeedbackModal extends Component {
             crop: crop
         })
     }
-    
+
     onImageLoaded = (image) => {
         this.imageRef = image;
-    }
-
-    handleImageLoaded(image) {
-        console.log("image", image)
     }
 
     handleOnCropComplete(crop, pixelCrop) {
@@ -62,37 +80,62 @@ class FeedbackModal extends Component {
         // const canvasRef = this.imagePreviewCanvasRef.current
         getCroppedImg(this.imageRef, crop, pixelCrop).then((croppedImage) => {
             console.log(croppedImage);
+            var reader = new FileReader();
+            var self = this;
+            reader.readAsDataURL(croppedImage);
+            reader.onloadend = function () {
+                self.handleCropImage(reader.result);
+            }
             this.setState({ croppedImage: URL.createObjectURL(croppedImage) })
         })
 
     }
+
+    handleCropImage(result) {
+        this.state.fields.coverPhoto = result;
+        console.log("new image", this.state.fields.coverPhoto);
+    }
+
+    handleChange = (field, e) => {
+        let fields = this.state.fields;
+        fields[field] = e.target.value;
+        this.setState({ fields });
+    }
+
     takeScreenShot() {
         html2canvas(document.querySelector('body')).then((canvas) => {
             this.screenShotImage = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-            this.setState({showFeedbackModal : true})
+            this.setState({ showFeedbackModal: true })
         });
     }
 
     render() {
-        
         return (
             <div>
                 <Modal id="myModal" isOpen={this.state.showFeedbackModal} toggle={this.toggle} fade={false} style={{ width: "100%", display: "block", opacity: 1 }}>
                     <ModalHeader toggle={this.toggle}>Send Feedback</ModalHeader>
                     <ModalBody>
-
                         <Form>
                             <FormGroup>
-                                <Input type="textarea" name="text" id="exampleText" placeholder="Describe your issue or share your ideas" />
+                                <Input type="text" name="text" id="Text"
+                                    placeholder="Please write summary"
+                                    onChange={this.handleChange.bind(this, "summary")}
+                                    value={this.state.fields["summary"]}
+                                    style={{ marginBottom: "18px" }}
+                                />
+                                <Input type="textarea" name="text" id="exampleText"
+                                    placeholder="Describe your issue or share your ideas"
+                                    onChange={this.handleChange.bind(this, "description")}
+                                    value={this.state.fields["description"]}
+                                />
                             </FormGroup>
                             <FormGroup check>
-                                <Input type="checkbox" />{'    '}
-                                <span style={{marginRight: "20px"}}>Include Screenshot</span>
+                                <Input onClick={this.state.toInclude} onClick={() => this.setState({ toInclude: !this.state.toInclude })} type="checkbox" style={{ left: "0px" }} defaultChecked/>{'    '}
+                                <span style={{ marginLeft: "10px" }}>Include Screenshot</span>
                             </FormGroup>
                             <hr />
                         </Form>
                         <div className='image'>
-                            {/* <img src={Image} alt="logo" className="Image-demo" /> */}
                             <ReactCrop
                                 src={this.screenShotImage}
                                 crop={this.state.crop}
@@ -104,21 +147,17 @@ class FeedbackModal extends Component {
                             <br />
                             <p>Crop image</p>
                             <img src={this.state.croppedImage} />
-                            {/* <canvas ref={this.imagePreviewCanvasRef}></canvas> */}
                         </div>
                     </ModalBody>
                     <ModalFooter>
                         <Button color="primary" onClick={this.toggle}>Cancel</Button>{' '}
-                        <Button color="secondary" onClick={this.toggle}>Send</Button>
-                        
+                        <Button color="secondary" onClick={this.handleSubmit}>Send</Button>
                     </ModalFooter>
                 </Modal>
             </div>
         )
     }
 }
-
-
 
 export default FeedbackModal;
 
