38d3f60f713ffc85037a02cc93faeb26596bcee7
Neosoft <neosoft@Neosofts-Mac-mini-7.local>
2019-05-21 21:09:09 +0530

    Added build folder to git repository

---
+++ build/precache-manifest.15bb2b90058858b4b1b75d38da2835a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba82d3ca33524f47ad151d1682f1b394",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c7a36654eadb5645a7b",
+    "url": "/static/css/2.1d5443d6.chunk.css"
+  },
+  {
+    "revision": "1b450e6dc7ea31d90e52",
+    "url": "/static/css/main.d6c771be.chunk.css"
+  },
+  {
+    "revision": "0c7a36654eadb5645a7b",
+    "url": "/static/js/2.e75fd83b.chunk.js"
+  },
+  {
+    "revision": "1b450e6dc7ea31d90e52",
+    "url": "/static/js/main.963f2304.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
